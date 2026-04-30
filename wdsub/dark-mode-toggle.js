14b3b9d19fda6d5e9bd30fba293010e1fffcf855
yanferguson <student.nf@youngsvilleacademy.org>
2026-04-30 10:37:13 -0400

    Update dark-mode-toggle.js

--- wdsub/dark-mode-toggle.js
+++ wdsub/dark-mode-toggle.js
@@ -1,30 +1,33 @@
   
   //determine if lightmode is on
-  let darkModeTgl = document.getElementById("darkModeTgl");
+  let darkModeTgl = document.getElementById("dark-mode-tgl");
   let onOffStatus = document.getElementById("on-off-status");
+
+darkModeTgl.addEventListener("click",() => mngMode());
   
   let isLight = true;
   
-  function mngMode(event){
-    
+  function mngMode(){
+   
+    isLight = !isLight;
   
   //change backround color to black if lightmode,else white
   
-  if (isLight)document.body.style.backgroundColor="black";
+  if (isLight) document.body.style.backgroundColor="black";
     else document.body.style.backgroundColor="white";
  
     //change text color to white if else black
     
-  if(islight) document.body.style.color="white";
- 
-    else document.body.style.color="black";
+  if (isLight) darkModeTgl.style.color="black",
+    onOffStatus.style.color="white";
+ else darkModeTgl.style.color="white",
+   onOffStatus.style.color="black";
 
     //change text to "darkmode on" if light mode, else "dark mode"
   
- if(islight) onOffStatus.innerHTML = "<p>dark mode on</p>" ;
-    else onOffStatus.innerHTML = "<p>dark mode off</p>"   ;
+ if(isLight) onOffStatus.innerHTML = "<p> dark mode on </p>" ;
+    else onOffStatus.innerHTML = "<p> dark mode off </p>"   ;
   
-  // flip the is light switch 
-  isLight = !isLight;
+
   }
  
