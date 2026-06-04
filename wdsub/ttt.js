1c6eee3316f3b3a25ece2b44c937b570631236e4
yanferguson <student.nf@youngsvilleacademy.org>
2026-06-04 11:27:02 -0400

    Update ttt.js

--- wdsub/ttt.js
+++ wdsub/ttt.js
@@ -1,58 +1,119 @@
 let gameOverDiv = document.getElementById("game-over");
-let winnerText = document.querySelector("game-over p");
+let winnerText = document.querySelector("#game-over p");
 let squareArr = document.querySelectorAll(".ttt-square");
+
 let currentPlayer = "X";
 
+for (const element of squareArr) {
+  element.addEventListener("click", (event) => drawSymbol(event));
+}
+
+/*
+*
+*/
 function drawSymbol(event) {
   let clickedSquare = event.target;
-  
-  
-  if (clickedSquare.innerText == ""){
-   clickedSquare.innerText = currentPlayer;
-    if (checkForWinner () == false) checkForDraw();
-    changePlayer();
+
+  if (clickedSquare.innerText == "") {
+    clickedSquare.innerText = currentPlayer;
+
+    if (checkForWinner()) endGame(false);
+    else if (checkForDraw()) endGame(true);
+    else changePlayer();
+  }
 }
 
+/*
+*
+*/
+function changePlayer() {
+  if (currentPlayer == "X") currentPlayer = "O";
+  else currentPlayer = "X";
+}
 
+/*
+* return: true if somebody wins
+*/
 function checkForWinner() {
- let isGameOver = false;
-}
-  
+  let isGameOver = false;
+
+  // Horizontal rows
   for (let i = 0; i < 3; i++) {
-    if (squareArr[3 * i].innerText == 
-        && squareArr[3 * i + 1].innerText == isGameOver
-        && squareArr[3 * i + 2].innerText == isGameOver) isGameOver = true;
+    if (
+      squareArr[3 * i].innerText == currentPlayer &&
+      squareArr[3 * i + 1].innerText == currentPlayer &&
+      squareArr[3 * i + 2].innerText == currentPlayer
+    ) {
+      isGameOver = true;
+    }
   }
-  
 
+  // Vertical columns
   for (let i = 0; i < 3; i++) {
-    if (squareArr[i].innerText == currentPlayer
-        && squareArr[i + 3].innerText == currentPlayer
-        && squareArr[i + 6].innerText == currentPlayer)  isGameOver = true;
+    if (
+      squareArr[i].innerText == currentPlayer &&
+      squareArr[i + 3].innerText == currentPlayer &&
+      squareArr[i + 6].innerText == currentPlayer
+    ) {
+      isGameOver = true;
+    }
   }
-  
-  if (squareArr[0].innerText == isGameOver
-      && squareArr[4].innerText == isGameOver
-      && squareArr[8].innerText == isGameOver) currentPlayer = true;
-  else if (squareArr[2].innerText == isGameOver
-      && squareArr[4].innerText == isGameOver
-      && squareArr[6].innerText == isGameOver) currentPlayer = true;
-  
-  return ;
+
+  // Diagonals
+  if (
+    squareArr[0].innerText == currentPlayer &&
+    squareArr[4].innerText == currentPlayer &&
+    squareArr[8].innerText == currentPlayer
+  ) {
+    isGameOver = true;
+  }
+
+  if (
+    squareArr[2].innerText == currentPlayer &&
+    squareArr[4].innerText == currentPlayer &&
+    squareArr[6].innerText == currentPlayer
+  ) {
+    isGameOver = true;
+  }
+
+  return isGameOver;
 }
 
+/*
+* return: true if board is full
+*/
+function checkForDraw() {
+  let isDraw = true;
 
+  for (const element of squareArr) {
+    if (element.innerText == "") isDraw = false;
+  }
 
+  return isDraw;
+}
 
+/*
+*
+*/
 function endGame(isDraw) {
   if (isDraw) {
-    winText.innerText = "Draw!";
+    winnerText.innerText = "Draw!";
   } else {
-    winText.innerText = "🎉" + ((currentPlayer == 0) ? "X" : "O") + " Player Wins!🎉";
-    incrementScore();
+    winnerText.innerText = "🎉 Player " + currentPlayer + " Won! 🎉";
   }
-  
-  endDiv.style.display = "block";
+
+  gameOverDiv.style.display = "block";
 }
 
+/*
+*
+*/
+function resetGame() {
+  for (const element of squareArr) {
+    element.innerText = "";
+  }
 
+  currentPlayer = "X";
+
+  gameOverDiv.style.display = "none";
+}
